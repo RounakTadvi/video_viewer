5380b6921ff0cfe3f7deec7e278ecbc06a3449b3
RounakTadvi <rounaktadvi786@gmail.com>
2021-07-16 16:05:25 +0530

    fix(video_landscape): video landscape orientation issue fixed

--- lib/ui/video_core/widgets/orientation.dart
+++ lib/ui/video_core/widgets/orientation.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_viewer/data/repositories/video.dart';
 
 class VideoCoreOrientation extends StatelessWidget {
-  const VideoCoreOrientation({Key? key, this.builder}) : super(key: key);
+  const VideoCoreOrientation({Key? key, required this.builder})
+      : super(key: key);
 
-  final Widget Function(bool)? builder;
+  final Widget Function(bool) builder;
+
+  void _setPreferredOrientation(BuildContext context) {
+    final video = VideoQuery().video(context, listen: false);
+    if (video.isFullScreen) {
+      SystemChrome.setPreferredOrientations(
+        [
+          DeviceOrientation.landscapeLeft,
+          DeviceOrientation.landscapeRight,
+        ],
+      );
+    } else {
+      SystemChrome.setPreferredOrientations(
+        [
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ],
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext _) {
     return OrientationBuilder(builder: (context, Orientation orientation) {
       final video = VideoQuery().video(context, listen: false);
-      return builder!(
+      debugPrint('isVideoFullScreen: ${video.isFullScreen}');
+      debugPrint('Orientation: $orientation');
+      _setPreferredOrientation(context);
+      
+      return builder(
         video.isFullScreen && orientation == Orientation.landscape,
       );
     });
